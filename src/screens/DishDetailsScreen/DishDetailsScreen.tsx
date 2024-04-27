b1dc00a785f552f64fea03c7870b034535eb670c
Muhammad Yousif <muhammad@aboidrees.dev>
2024-04-27 13:43:10 +0300

    code refactor

--- src/screens/DishDetailsScreen/DishDetailsScreen.tsx
+++ src/screens/DishDetailsScreen/DishDetailsScreen.tsx
@@ -59,23 +59,43 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     borderRadius: 2,
   },
-  name: {fontSize: 30, fontWeight: "600", marginVertical: 10},
-  description: {color: "gray", fontSize: 16, writingDirection: "ltr", textAlign: "justify"},
-  separator: {height: 1, backgroundColor: "lightgrey", marginVertical: 10},
+  name: {
+    fontSize: 30,
+    fontWeight: "600",
+    marginVertical: 10
+  },
+  description: {
+    color: "gray",
+    fontSize: 16,
+    writingDirection: "ltr",
+    textAlign: "justify"
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "lightgrey",
+    marginVertical: 10
+  },
   row: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     marginTop: 50,
   },
-  quantity: {fontSize: 25, marginHorizontal: 20},
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20
+  },
   button: {
     backgroundColor: "black",
     marginTop: "auto",
     padding: 20,
     alignItems: "center",
   },
-  buttonText: {color: "white", fontWeight: "600", fontSize: 18},
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 18
+  },
 });
 
 
--- src/screens/RestaurantDetailsScreen/RestaurantDetailsScreen.tsx
+++ src/screens/RestaurantDetailsScreen/RestaurantDetailsScreen.tsx
@@ -62,19 +62,43 @@ const RestaurantDetailsScreen = () => {
 
 const styles = StyleSheet.create({
   page: {flex: 1},
-  iconContainer: {position: "absolute", top: 40, left: 10},
-  image: {width: "100%", aspectRatio: 5 / 3},
-  title: {fontSize: 35, fontWeight: "600", marginVertical: 10},
-  menuTitle: {marginTop: 20, fontSize: 18, letterSpacing: 0.7},
-  subtitle: {fontSize: 15, color: "#525252"},
-  container: {margin: 10},
+  iconContainer: {
+    position: "absolute",
+    top: 40,
+    left: 10
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "600",
+    marginVertical: 10
+  },
+  menuTitle: {
+    marginTop: 20,
+    fontSize: 18,
+    letterSpacing: 0.7
+  },
+  subtitle: {
+    fontSize: 15,
+    color: "#525252"
+  },
+  container: {
+    margin: 10
+  },
   button: {
     backgroundColor: "black",
     marginTop: "auto",
     padding: 20,
     alignItems: "center",
   },
-  buttonText: {color: "white", fontWeight: "600", fontSize: 18},
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 18
+  },
 });
 
 
