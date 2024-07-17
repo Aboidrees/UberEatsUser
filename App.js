c31a81dadde68171c88ff92bd94846ad3ddb7722
Muhammad Yousif <m.alshf3ee@gmail.com>
2024-07-17 19:04:43 +0300

    checkpoint

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { Amplify } from "aws-amplify";
-import RootNavigator from "./src/navigation";
-import { AuthContextProvider, BasketContextProvider } from "./src/context/";
-import { OrderContextProvider } from "./src/context/OrderContext";
-import { withAuthenticator } from "@aws-amplify/ui-react-native";
+import {Amplify} from "aws-amplify";
+import {StatusBar} from "expo-status-bar";
+import {NavigationContainer} from "@react-navigation/native";
+import {withAuthenticator} from "@aws-amplify/ui-react-native";
 
+import {AuthContextProvider, BasketContextProvider, OrderContextProvider} from "./src/context/";
+import RootNavigator from "./src/navigation";
 import AmplifyConfig from "./src/amplifyconfiguration.json";
 
 Amplify.configure(AmplifyConfig);
@@ -15,11 +14,11 @@ const App = () => (
     <AuthContextProvider>
       <BasketContextProvider>
         <OrderContextProvider>
-          <RootNavigator />
+          <RootNavigator/>
         </OrderContextProvider>
       </BasketContextProvider>
     </AuthContextProvider>
-    <StatusBar style="dark" />
+    <StatusBar style="dark"/>
   </NavigationContainer>
 );
 
